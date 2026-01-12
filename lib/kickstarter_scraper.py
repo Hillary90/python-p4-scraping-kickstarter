9263f6125be58610f4b296e240dc9306e9b2a3ce
Hillary Tanui <hillarytanui80@gmail.com>
2026-01-12 22:03:37 +0300

    completed assignment

--- lib/kickstarter_scraper.py
+++ lib/kickstarter_scraper.py
@@ -12,6 +12,7 @@ def create_project_dict():
     html = ''
     with open('./fixtures/kickstarter.html') as file:
         html = file.read()
+
     kickstarter = BeautifulSoup(html, 'html.parser')
     projects = {}
     # Iterate through the projects
@@ -27,5 +28,7 @@ def create_project_dict():
 
     return projects
 
-projects = create_project_dict()
-print(projects)
+if __name__ == "__main__":
+    ipdb.set_trace()
+    create_project_dict()
+
